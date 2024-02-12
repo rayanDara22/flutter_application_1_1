f43cdc8340d7481c69af7f86f209849d78e928ba
rayanDara22 <panadara28@gmail.com>
2024-02-12 22:35:42 +0300

    اا

--- lib/Feature/auth/Csreen/logIn.dart
+++ lib/Feature/auth/Csreen/logIn.dart
@@ -29,7 +29,7 @@ class LoginPage extends StatelessWidget {
     return const Column(
       children: [
         Text(
-          "لەچیggای",
+          "بەخێربێیت",
           style: TextStyle(
               fontFamily: "rudaw",
               fontSize: 40,
